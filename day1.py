38312a6c11b01160d75d7ec4cd283bc5244fd7a1
DoubleZed <zintis.zilbers@gmail.com>
2021-12-04 22:59:24 +0100

    Added Part Two of Day 1

--- day1.py
+++ day1.py
@@ -1,11 +1,33 @@
 import os
-
-inputPath = os.path.join(os.getcwd(),"input","input_day1.txt")
+import pandas as pd
+### Part one ###
+""" inputPath = os.path.join(os.getcwd(),"input","input_day1.txt")
 measureIncrease = 0
 measurementList = [line.rstrip() for line in open(inputPath,"r")]
 increaseCount = 0
 for x,y in zip(measurementList, measurementList[1:]):
     if int(x) < int(y):
         increaseCount += 1
+print (increaseCount) """
+
+
+### Part two ###
+inputPath = os.path.join(os.getcwd(),"input","input_day1.txt")
+measureIncrease = 0
+measurementList = [int(line.rstrip()) for line in open(inputPath,"r")]
+threeMeasurementList = pd.DataFrame(
+    {'1st':measurementList[:-2],
+     '2nd':measurementList[1:-1],
+     '3rd':measurementList[2:]
+    })
+
+sumList = []
+for i in range(len(threeMeasurementList)):
+    sum = threeMeasurementList.loc[i]['1st'] + threeMeasurementList.loc[i]['2nd'] + threeMeasurementList.loc[i]['3rd']
+    sumList.append(int(sum))
+
+increaseCount = 0
+for index, value in enumerate(sumList[:-1]):
+    if value < sumList[index+1]:
+        increaseCount += 1
 print (increaseCount)
-       
