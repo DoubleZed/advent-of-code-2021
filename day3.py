a89863d7593e94036396e12dcfa533e97558d7c4
DoubleZed <zintis.zilbers@gmail.com>
2021-12-09 20:29:05 +0100

    Added Gamma Rate lookup

--- day3.py
+++ day3.py
@@ -4,18 +4,8 @@ import collections
 inputPath = os.path.join(os.getcwd(),"input","input_day3.txt")
 lineInput = [line.rstrip() for line in open(inputPath,"r")]
 numLines = sum(1 for line in lineInput)
-#print(numLines)
-lineList = []
-transposedList  = []
-bitList = []
-#print(lineInput[0])
-#print(lineInput[0][4])
 
-#for i in range(numLines-1):
-#    for j in range(len(lineInput[0])):
-#        if lineInput[i][j]
- #  if lineInput[]
-
+#Getting Gamma Rate
 gammaRate = ""
 for i in range(len(lineInput[0])):
     zeros = 0
@@ -25,8 +15,6 @@ for i in range(len(lineInput[0])):
             zeros += 1
         elif lineInput[j][i] == "1":
             ones += 1
-    #print (zeros)
-    #print (ones)
     if zeros > ones:
         gammaRate += str(0)
     else:
@@ -34,31 +22,3 @@ for i in range(len(lineInput[0])):
 
 print(gammaRate)
 print(int(gammaRate, 2))
-
-#Binary to int:
-#int(b, 2)
-        
-
-
-
-
-#    for i in range(len(item)):
-
-   #     zeros = 0
-  #      ones = 0
-#        if item[i] == 0:
-#            int(zeros) += 1
-
-
-
-#    nBits = ""
-#    for i in range(len(item)):
-  #      nBits+=str(item[i])
-    #print(nBits)
-    #lineList.append(item.split())
-#    transposedList.append(nBits)
-    #print (item[0:1])
-#print (transposedList)
-
-#print (lineInput)
-#print (lineList)
